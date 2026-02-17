064e61e090f9b07f57486856a898a7e4d33dd8a3
Mohamed_Ali_Shaltoot <mohamedali3092002@gmail.com>
2026-02-17 23:08:16 +0200

    rename favoueite screen

--- app/src/main/java/com/example/skylogic/models/Screen.kt
+++ app/src/main/java/com/example/skylogic/models/Screen.kt
@@ -1,2 +1,9 @@
-package com.example.skylogic.models 
+package com.example.skylogic.models
 
+sealed class Screen(val route: String) {
+    object Home : Screen("home")
+    object Favourite : Screen("favourite")
+    object Alerts : Screen("alerts")
+    object Settings : Screen("settings")
+    object MapSelection : Screen("map_selection")
+}
