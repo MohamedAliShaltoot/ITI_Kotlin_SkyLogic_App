921f157cc315d841fee96b634d1b6bde89d49035
Mohamed_Ali_Shaltoot <mohamedali3092002@gmail.com>
2026-02-18 10:27:38 +0200

    other files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.devtools.ksp")
+
 }
 
 android {
@@ -82,5 +84,12 @@ dependencies {
     implementation("com.google.maps.android:maps-compose:2.11.4")
     implementation("com.google.android.gms:play-services-maps:18.2.0")
     implementation("org.osmdroid:osmdroid-android:6.1.16")
+
+    // Room
+    val room_version = "2.8.4"
+    implementation("androidx.room:room-ktx:${room_version}")
+    implementation("androidx.room:room-runtime:$room_version")
+    ksp("androidx.room:room-compiler:$room_version")
+
 
 }
