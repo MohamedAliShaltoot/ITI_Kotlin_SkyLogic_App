6b5b30d14c29e1bb48ca63cde6dca373ea06388b
Mohamed_Ali_Shaltoot <mohamedali3092002@gmail.com>
2026-02-18 10:26:38 +0200

    add fav button to selectionMapScreen

--- app/src/main/java/com/example/skylogic/view/mapSelectionView/MapSelectionScreen.kt
+++ app/src/main/java/com/example/skylogic/view/mapSelectionView/MapSelectionScreen.kt
@@ -31,13 +31,19 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Divider
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.SegmentedButtonDefaults.Icon
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.skylogic.models.Screen
 import com.example.skylogic.view.settingView.settingViewModel.SettingsViewModel
 import com.example.skylogic.models.GeoResponse
+import com.example.skylogic.view.favouriteView.FavoriteViewModel
 import com.example.skylogic.view.mapSelectionView.mapSelectionViewModel.MapSelectionViewModel
 import com.example.skylogic.view.mapSelectionView.mapSelectionViewModel.MapSelectionViewModelFactory
 import com.example.skylogic.view.weatherView.weatherViewModel.WeatherViewModel
@@ -48,8 +54,10 @@ import kotlinx.coroutines.launch
 fun MapSelectionScreen(
     navController: NavController,
     settingsViewModel: SettingsViewModel,
-    weatherViewModel: WeatherViewModel
-) {
+    weatherViewModel: WeatherViewModel,
+    favoriteViewModel: FavoriteViewModel
+)
+ {
 
     val context = LocalContext.current
 
@@ -193,6 +201,30 @@ fun MapSelectionScreen(
                 }
             }
         }
+        FloatingActionButton(
+            onClick = {
+                selectedPoint?.let {
+
+                    val cityName = searchQuery.ifBlank { "Custom Location" }
+
+                    favoriteViewModel.addFavorite(
+                        name = cityName,
+                        lat = it.latitude,
+                        lon = it.longitude
+                    )
+                }
+            },
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(end = 20.dp, bottom = 90.dp),
+            containerColor = Color.Red
+        ) {
+            Icon(
+                imageVector = Icons.Default.Favorite,
+                contentDescription = "Add to favorites",
+                tint = Color.White
+            )
+        }
 
         Button(
             onClick = {
@@ -212,212 +244,9 @@ fun MapSelectionScreen(
                 containerColor = Color(0xFF00C853)
             ),
             shape = RoundedCornerShape(16.dp)
-        ) {
+        )
+        {
             Text("Confirm Location", color = Color.White)
         }
     }
 }
-
-//@Composable
-//fun MapSelectionScreen(
-//    navController: NavController,
-//    settingsViewModel: SettingsViewModel,
-//    viewModel: WeatherViewModel
-//) {
-//
-//    val context = LocalContext.current
-//    val coroutineScope = rememberCoroutineScope()
-//
-//    var selectedPoint by remember { mutableStateOf<GeoPoint?>(null) }
-//    var marker by remember { mutableStateOf<Marker?>(null) }
-//    var searchQuery by remember { mutableStateOf("") }
-//    var mapView by remember { mutableStateOf<MapView?>(null) }
-//    var suggestions by remember { mutableStateOf<List<GeoResponse>>(emptyList()) }
-//
-//    LaunchedEffect(searchQuery) {
-//
-//        if (searchQuery.length < 2) {
-//            suggestions = emptyList()
-//            return@LaunchedEffect
-//        }
-//
-//        delay(400)
-//
-//        try {
-//            val result = viewModel.getCityCoordinates(searchQuery)
-//            suggestions = result
-//        } catch (e: Exception) {
-//            suggestions = emptyList()
-//        }
-//    }
-//
-//    Box(Modifier.fillMaxSize()) {
-//
-//        AndroidView(
-//            modifier = Modifier.fillMaxSize(),
-//            factory = {
-//
-//                Configuration.getInstance()
-//                    .load(context, context.getSharedPreferences("osm", Context.MODE_PRIVATE))
-//
-//                val mv = MapView(context)
-//                mapView = mv
-//
-//                mv.setTileSource(TileSourceFactory.MAPNIK)
-//                mv.setMultiTouchControls(true)
-//                mv.zoomController.setVisibility(
-//                    CustomZoomButtonsController.Visibility.ALWAYS
-//                )
-//
-//                val controller = mv.controller
-//                controller.setZoom(5.0)
-//                controller.setCenter(GeoPoint(30.0444, 31.2357))
-//                val mapEventsReceiver = object : MapEventsReceiver {
-//
-//                    override fun singleTapConfirmedHelper(p: GeoPoint?): Boolean {
-//
-//                        p?.let { geoPoint ->
-//
-//                            selectedPoint = geoPoint
-//
-//                            coroutineScope.launch {
-//
-//                                try {
-//                                    val result = viewModel.reverseGeocode(
-//                                        geoPoint.latitude,
-//                                        geoPoint.longitude
-//                                    )
-//
-//                                    val cityName =
-//                                        if (result.isNotEmpty())
-//                                            result[0].name
-//                                        else
-//                                            "Selected Location"
-//
-//                                    marker?.let { mv.overlays.remove(it) }
-//
-//                                    val newMarker = Marker(mv)
-//                                    newMarker.position = geoPoint
-//                                    newMarker.setAnchor(
-//                                        Marker.ANCHOR_CENTER,
-//                                        Marker.ANCHOR_BOTTOM
-//                                    )
-//                                    newMarker.title = cityName
-//                                    newMarker.showInfoWindow()
-//
-//                                    mv.overlays.add(newMarker)
-//                                    marker = newMarker
-//                                    mv.invalidate()
-//
-//                                } catch (_: Exception) {}
-//                            }
-//                        }
-//
-//                        return true
-//                    }
-//
-//                    override fun longPressHelper(p: GeoPoint?) = false
-//                }
-//
-//                mv.overlays.add(MapEventsOverlay(mapEventsReceiver))
-//                mv
-//            }
-//        )
-//        Column(
-//            Modifier
-//                .align(Alignment.TopCenter)
-//                .padding(16.dp)
-//        ) {
-//
-//            TextField(
-//                value = searchQuery,
-//                onValueChange = { searchQuery = it },
-//                modifier = Modifier.fillMaxWidth(),
-//                placeholder = { Text("Search city...") },
-//                shape = RoundedCornerShape(20.dp)
-//            )
-//
-//            if (suggestions.isNotEmpty()) {
-//
-//                Card(
-//                    shape = RoundedCornerShape(16.dp),
-//                    elevation = CardDefaults.cardElevation(8.dp)
-//                ) {
-//
-//                    LazyColumn(
-//                        modifier = Modifier
-//                            .fillMaxWidth()
-//                            .heightIn(max = 300.dp)
-//                    ) {
-//
-//                        items(suggestions) { city ->
-//
-//                            Text(
-//                                text = "${city.name}, ${city.country}",
-//                                modifier = Modifier
-//                                    .fillMaxWidth()
-//                                    .clickable {
-//
-//                                        val geoPoint =
-//                                            GeoPoint(city.lat, city.lon)
-//
-//                                        selectedPoint = geoPoint
-//                                        searchQuery =
-//                                            "${city.name}, ${city.country}"
-//                                        suggestions = emptyList()
-//
-//                                        mapView?.controller?.animateTo(geoPoint)
-//                                        mapView?.controller?.setZoom(12.0)
-//
-//                                        marker?.let {
-//                                            mapView?.overlays?.remove(it)
-//                                        }
-//
-//                                        val newMarker = Marker(mapView)
-//                                        newMarker.position = geoPoint
-//                                        newMarker.setAnchor(
-//                                            Marker.ANCHOR_CENTER,
-//                                            Marker.ANCHOR_BOTTOM
-//                                        )
-//                                        newMarker.title =
-//                                            "${city.name}, ${city.country}"
-//                                        newMarker.showInfoWindow()
-//
-//                                        mapView?.overlays?.add(newMarker)
-//                                        marker = newMarker
-//                                        mapView?.invalidate()
-//                                    }
-//                                    .padding(14.dp)
-//                            )
-//
-//                            Divider()
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//        Button(
-//            onClick = {
-//                selectedPoint?.let {
-//
-//                    settingsViewModel.setCustomLocation(
-//                        it.latitude,
-//                        it.longitude
-//                    )
-//                    settingsViewModel.setLocationMode("Map")
-//                    navController.navigate(Screen.Home.route)
-//                }
-//            },
-//            modifier = Modifier
-//                .align(Alignment.BottomCenter)
-//                .padding(20.dp),
-//            colors = ButtonDefaults.buttonColors(
-//                containerColor = Color(0xFF00C853)
-//            ),
-//            shape = RoundedCornerShape(16.dp)
-//        ) {
-//            Text("Confirm Location", color = Color.White)
-//        }
-//    }
-//}
-
