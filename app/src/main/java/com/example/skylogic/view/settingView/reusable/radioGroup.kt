67c6973fe2b1dd6c37709fc4e23e3166820362d7
Mohamed_Ali_Shaltoot <mohamedali3092002@gmail.com>
2026-02-28 01:10:03 +0200

    modify setting view to support full arabic and give notification permission

--- app/src/main/java/com/example/skylogic/view/settingView/reusable/radioGroup.kt
+++ app/src/main/java/com/example/skylogic/view/settingView/reusable/radioGroup.kt
@@ -16,43 +16,39 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
+data class RadioOption(val key: String, val label: String)
+
 @Composable
 fun RadioGroup(
-    options: List<String>,
+    options: List<RadioOption>,
     selected: String,
     onSelect: (String) -> Unit
 ) {
-
     Column {
         options.forEach { option ->
-
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .clickable { onSelect(option) }
+                    .clickable { onSelect(option.key) }
                     .padding(vertical = 8.dp),
                 verticalAlignment = Alignment.CenterVertically
             ) {
-
                 RadioButton(
-                    selected = option == selected,
-                    onClick = { onSelect(option) },
+                    selected = option.key == selected,
+                    onClick = { onSelect(option.key) },
                     colors = RadioButtonDefaults.colors(
                         selectedColor = Color(0xFF4DA3FF),
                         unselectedColor = Color.White.copy(alpha = 0.6f)
                     )
                 )
-
-
                 Spacer(Modifier.width(8.dp))
                 Text(
-                    option,
-                    color = if (option == selected)
+                    text = option.label,
+                    color = if (option.key == selected)
                         Color(0xFF4DA3FF)
                     else
                         Color.White
                 )
-
             }
         }
     }
